161e429172fbb2b1584fae3b8a90107d4cf35d42
Hannes Hertach <hanneshertach@gmail.com>
2022-09-20 16:29:04 +0200

    feat: implement video hero block

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -656,13 +656,9 @@ function loadFooter(footer) {
  * Builds all synthetic blocks in a container element.
  * @param {Element} main The container element
  */
+// eslint-disable-next-line no-unused-vars
 function buildAutoBlocks(main) {
-  try {
-    buildHeroBlock(main);
-  } catch (error) {
-    // eslint-disable-next-line no-console
-    console.error('Auto Blocking failed', error);
-  }
+
 }
 
 /**
