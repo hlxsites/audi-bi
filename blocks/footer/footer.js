1dc5dfa3f6b44a0f566dde147ca3c19e7edaf4a1
Hannes Hertach <hanneshertach@gmail.com>
2022-09-21 12:55:55 +0200

    add back to top button

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -16,4 +16,15 @@ export default async function decorate(block) {
   footer.innerHTML = html;
   await decorateIcons(footer);
   block.append(footer);
+
+  block.querySelector('div > div > div').classList.add('footer-content-container');
+  block.querySelector('div > div').classList.add('footer-container');
+
+  // Back to top button
+  const $backToTop = document.createElement('button');
+  $backToTop.innerHTML = '<p>Back to top</p><svg height="24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M7 6l4.5-4.5L16 6m-4.5 18V1.5" fill="none" stroke="currentColor" stroke-miterlimit="10"></path></svg>';
+  $backToTop.addEventListener('click', () => {
+    window.scrollTo(0, 0);
+  })
+  block.querySelector('div > div').prepend($backToTop);
 }
