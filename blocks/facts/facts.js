f963fe30810c9ac504d1bdc3e02f6dca466b24ea
Hannes Hertach <hanneshertach@gmail.com>
2022-09-21 12:17:07 +0200

    implement main page content styling

--- blocks/facts/facts.js
+++ blocks/facts/facts.js
@@ -1,5 +1,22 @@
 export default async function decorate($block) {
   $block.children[0]?.classList.add('facts-title-container');
-  $block.children[1]?.classList.add('facts-row');
-  $block.children[2]?.classList.add('facts-row');
+
+  const rows = [...$block.children].slice(1).filter((x) => !!x);
+
+  rows.forEach((row) => {
+    row.classList.add('facts-row');
+
+    const rowItems = [...row.children];
+    rowItems.forEach((rowItem) => {
+      if (rowItem.children.length === 3) {
+        const unit = rowItem.children[1];
+        const unitText = unit.textContent;
+        const $unitSpan = document.createElement('span');
+        $unitSpan.textContent = ` ${unitText}`;
+        $unitSpan.classList.add('unit');
+        rowItem.children[0].append($unitSpan);
+        rowItem.removeChild(rowItem.children[1]);
+      }
+    });
+  });
 }
